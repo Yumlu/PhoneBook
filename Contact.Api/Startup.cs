32b890a4ea63a46d5d2c79e2ee106ee8414a6fa5
omer <mercel74@gmail.com>
2021-03-14 21:21:38 +0300

    Trrk

--- Contact.Api/Startup.cs
+++ Contact.Api/Startup.cs
@@ -23,7 +23,7 @@ namespace Contact.Api
         }
 
         public IConfiguration Configuration { get; }
-
+        //Týrriiiikk
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
