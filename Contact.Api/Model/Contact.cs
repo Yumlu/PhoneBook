7ebbc89fb5c70d2e7ff68a1bf5262423624243af
Mustafa Yümlü <m.yumlu90@gmail.com>
2021-03-14 20:56:07 +0300

    First

---
+++ Contact.Api/Model/Contact.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Contact.Api.Model
+{
+    public class Contact
+    { 
+        [BsonId] 
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string UUID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+        public ContactInfo ContactInfo { get; set; }
+       
+    }
+}
---
+++ Contact.Api/Model/ContactInfo.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact.Api.Model
+{
+    public class ContactInfo
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+    }
+}
